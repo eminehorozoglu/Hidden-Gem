9bda8a627a3910ce0c3eceaeae4af8052c4cfcea
eminehorozoglu <emineh86@gmail.com>
2025-02-26 09:44:37 +0000

    Changed return page button

--- src/app/return/page.js
+++ src/app/return/page.js
@@ -4,8 +4,8 @@ export default function ReturnPage (){
         <main className="flex min-h-screen flex-col justify-evenly p-10 items-center">
         <h1 className=" text-4xl text-amber-950">Your suggestion submitted. Thanks for your suggestion.</h1>
         <p className="flex flex-row  justify-evenly">
-        <Link href={"/"}><button className="bg-amber-600 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded-full"> Return Home</button></Link>
-        <Link href={"/suggest"}><button className="bg-amber-600 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded-full"> Give Another Suggest</button></Link>
+        <Link href={"/"}><button className="text-gray-800 text-center text-sm bg-gray-400 p-3 m-3 hover:bg-yellow-500 hover:scale-105 hover:brightness-110"> Return Home</button></Link>
+        <Link href={"/suggest"}><button className="text-gray-800 text-center text-sm bg-gray-400 p-3 m-3 hover:bg-yellow-500 hover:scale-105 hover:brightness-110"> Give Another Suggest</button></Link>
         </p>
         </main>
     )
